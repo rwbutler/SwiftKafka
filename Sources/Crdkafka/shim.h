a3263092c6519fa5271c7d34ead7f8c45b12285a
Ross Butler <ross.butler@gocity.com>
2023-04-14 13:34:31 +0100

    Updated for Apple Silicon

--- Sources/Crdkafka/shim.h
+++ Sources/Crdkafka/shim.h
@@ -1,7 +1,9 @@
 #ifndef rdkafka_shim_h
 #define rdkafka_shim_h
-#ifdef __APPLE__
+#ifdef __APPLE__ && __x86_64__
 #include "/usr/local/include/librdkafka/rdkafka.h"
+#elif __APPLE__ && __aarch64__
+#include "/opt/homebrew/include/librdkafka/rdkafka.h"
 #else
 #include "/usr/include/librdkafka/rdkafka.h"
 #endif
